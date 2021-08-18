a7d207541175759f9606f70fc351b70d80c73987
Rudolfvh <rudolfvh@yandex.by>
2021-08-18 17:39:56 +0300

    PhraseAnalyser

---
+++ src/bel/matvey/theme/conditional/Task_02/PhraseAnalyserTest.java
@@ -0,0 +1,10 @@
+package bel.matvey.theme.conditional.Task_02;
+
+import bel.matvey.theme.conditional.Task_02.entity.PhraseAnalyser;
+
+public class PhraseAnalyserTest {
+    public static void main(String[] args) {
+        PhraseAnalyser analyser = new PhraseAnalyser();
+        System.out.println(analyser.analyse("Mak grat again"));
+    }
+}
