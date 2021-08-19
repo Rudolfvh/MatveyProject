adfaf7dcf13c5e61938ca2f24047390abbf1f030
Rudolfvh <rudolfvh@yandex.by>
2021-08-19 10:40:41 +0300

    Stock

---
+++ src/bel/matvey/theme/conditional/Task_04/Stock.java
@@ -0,0 +1,29 @@
+package bel.matvey.theme.conditional.Task_04;
+
+    public class Stock{
+
+    public int minPrice,maxPrice,currentPrice;
+
+    String company;
+
+            public Stock(String companyPrice, int current) {
+                this.minPrice = current;
+                this.maxPrice = current;
+                this.currentPrice = current;
+                this.company = companyPrice;
+            }
+
+            public void updatePrice (int stock){
+                if(this.maxPrice < stock)this.maxPrice = stock;
+                if(this.minPrice > stock)this.minPrice = stock;
+                this.currentPrice = stock;
+            }
+
+            public void printInformation () {
+                System.out.print("Company = " + '"' + this.company + '"' + ", ");
+                System.out.print("Current Price = " + currentPrice + ", ");
+                System.out.print("Min Price = " + this.minPrice + ", ");
+                System.out.println("Max Price = " + this.maxPrice);
+            }
+
+    }
---
+++ src/bel/matvey/theme/conditional/Task_04/entity/StockTest.java
@@ -0,0 +1,17 @@
+package bel.matvey.theme.conditional.Task_04.entity;
+
+import bel.matvey.theme.conditional.Task_04.Stock;
+
+public class StockTest {
+    public static void main(String[] args) {
+        Stock google = new Stock("GooG", 8);
+
+        google.printInformation();
+
+        google.updatePrice(15);
+        google.updatePrice(7);
+        google.updatePrice(14);
+
+        google.printInformation();
+    }
+}
