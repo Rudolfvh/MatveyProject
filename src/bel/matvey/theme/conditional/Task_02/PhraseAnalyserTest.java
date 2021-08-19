6cadf1173d3aa61f0011fc4e2e58e5d676d825ed
Rudolfvh <rudolfvh@yandex.by>
2021-08-19 11:21:36 +0300

    PhraseAnalyser

--- src/bel/matvey/theme/conditional/Task_02/PhraseAnalyserTest.java
+++ src/bel/matvey/theme/conditional/Task_02/PhraseAnalyserTest.java
@@ -1,5 +1,7 @@
 package bel.matvey.theme.conditional.Task_02;
 
+import bel.matvey.theme.conditional.Task_02.entity.PhraseAnalyser;
+
 public class PhraseAnalyserTest {
     public static void main(String[] args) {
         PhraseAnalyser analyser = new PhraseAnalyser();
