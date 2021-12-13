b957d8ee50cc4c333d2f8636202ce51ec0671b14
Rudolfvh <rudolfvh@yandex.by>
2021-12-13 15:53:56 +0300

    Counter

--- src/bel/matvey/theme/ConceptsOfObject/Task_01/CounterDemo.java
+++ src/bel/matvey/theme/ConceptsOfObject/Task_01/CounterDemo.java
@@ -1,2 +1,9 @@
-package bel.matvey.theme.ConceptsOfObject.Task_01;public class CounterDemo {
+package bel.matvey.theme.ConceptsOfObject.Task_01;
+
+import bel.matvey.theme.ConceptsOfObject.Task_01.entity.Counter;
+
+public class CounterDemo {
+    public static void main(String[] args) {
+        Counter counter = new Counter();
+    }
 }
