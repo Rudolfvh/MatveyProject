b99e87e18e574748bbe392ae3a2c45713c1ee012
Rudolfvh <89134031+Rudolfvh@users.noreply.github.com>
2021-12-13 15:43:56 +0300

    Delete src/bel/matvey/theme/ConceptsOfObject directory

--- src/bel/matvey/theme/ConceptsOfObject/Task_01/CounterDemo.java
+++
@@ -1,9 +0,0 @@
-package bel.matvey.theme.ConceptsOfObject.Task_01;
-
-import bel.matvey.theme.ConceptsOfObject.Task_01.entity.Counter;
-
-public class CounterDemo {
-    public static void main(String[] args) {
-        Counter counter = new Counter();
-    }
-}
